b1b05ea7b25f98429a88c14942d53e77b04d38a1
fogetwater <11042514+fogetwater@user.noreply.gitee.com>
2023-11-16 20:33:02 +0800

    新增home.vue

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-
+import router from './router/index'
 
 
 const app = createApp(App);
-
+app.use(router)
 
 app.use(ElementPlus)
 app.mount('#app')
