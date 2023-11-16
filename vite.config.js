499a275790e9d567a4934c8ea4b4d0b2360c5151
Ann <291388837@qq.com>
2023-11-16 18:30:21 +0800

    配置了axios和api解耦

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import AutoImport from 'unplugin-auto-import/vite'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        AutoImport({
+            imports:['vue','vue-router']//自动导入vue和vue-router相关函数
+        })
+    ],
     server: {
         proxy: {
             // 使用正则表达式
@@ -25,6 +32,7 @@ export default defineConfig({
                 target: 'http://evo-hu.xf-yun.com',
                 changeOrigin: true
             },
+            '/api':'http://testapi.xuexiluxian.cn'
             // '/':{
             //   target:''
             // }
